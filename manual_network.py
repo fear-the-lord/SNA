00909853dcecb011c1105447db40b997ab7e30d7
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-06-11 23:22:48 +0530

    node-names updated. checkStability updated

--- manual_network.py
+++ manual_network.py
@@ -21,10 +21,12 @@ def checkStability(tris_list, G):
     unstableNum = len(unstable)
 
     balance_score = stableNum / (stableNum + unstableNum)
-    if balance_score >=  0.9:
-        print('The network is STABLE')
+    if balance_score == 1.0:
+        print('The network is STRUCTURALLY BALANCED') 
+    elif balance_score >=  0.9:
+        print('The network is APPROXIMATELY STRUCTURALLY BALANCED')
     else:
-        print('The network is UNSTABLE')
+        print('The network is Not STRUCTURALLY BALANCED')
 
     print("Stable triangles: ", stableNum)
     print("Unstable triangles: ", unstableNum)
@@ -32,48 +34,51 @@ def checkStability(tris_list, G):
     return stable, unstable
 
 if __name__ == "__main__":
-  G = nx.Graph()
+    G = nx.Graph()
 
-  n = 7
-  G.add_nodes_from([i for i in range(1, n+1)])
-  mapping = {1:'Alexandra',2:'Anterim', 3:'Bercy',4:'Bearland',5:'Eplex', 6:'Ron', 7:'Harry'}
-
-  G = nx.relabel_nodes(G, mapping)
-  signs = ['+', '-']
-  for i in G.nodes():
-      for j in G.nodes():
-          if i != j:
-              G.add_edge(i, j, sign = random.choice(signs))
-
-  edges = G.edges()
-  nodes = G.nodes()
-
-  tris_list = [list(x) for x in itertools.combinations(nodes, 3)]
-  stable, unstable = checkStability(tris_list, G)
-
-  edge_labels = nx.get_edge_attributes(G, 'sign')
-  pos = nx.circular_layout(G)
-
-  nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-  nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 15)
-
-  Friends = [k for k, v in edge_labels.items() if v == '+']
-  Enemies = [k for k, v in edge_labels.items() if v == '-']
-  nx.draw_networkx_edges(G, pos, edgelist = Friends, edge_color="g")
-  nx.draw_networkx_edges(G, pos, edgelist = Enemies, edge_color="r")
+    n = 7
+    G.add_nodes_from([i for i in range(1, n+1)])
   
-  plt.pause(10)
+    mapping = {}
+    for i in range(1,n+1):
+        mapping[i] = "Node" + str(i)
+    G = nx.relabel_nodes(G, mapping)
 
-  for i in range(len(stable)):
-    temp = [tuple(x) for x in itertools.combinations(stable[i], 2)]
-    plt.cla()
-    nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-    nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="g", width = 6 )
-    plt.pause(2)
+    signs = ['+', '-']
+    for i in G.nodes():
+        for j in G.nodes():
+            if i != j:
+                G.add_edge(i, j, sign = random.choice(signs))
 
-  for i in range(len(unstable)):
-    temp = [tuple(x) for x in itertools.combinations(unstable[i], 2)]
-    plt.cla()
+    edges = G.edges()
+    nodes = G.nodes()
+
+    tris_list = [list(x) for x in itertools.combinations(nodes, 3)]
+    stable, unstable = checkStability(tris_list, G)
+
+    edge_labels = nx.get_edge_attributes(G, 'sign')
+    pos = nx.circular_layout(G)
+
     nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-    nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="r", width = 6)
-    plt.pause(2)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 15)
+
+    Friends = [k for k, v in edge_labels.items() if v == '+']
+    Enemies = [k for k, v in edge_labels.items() if v == '-']
+    nx.draw_networkx_edges(G, pos, edgelist = Friends, edge_color="g")
+    nx.draw_networkx_edges(G, pos, edgelist = Enemies, edge_color="r")
+  
+    plt.pause(10)
+
+    for i in range(len(stable)):
+        temp = [tuple(x) for x in itertools.combinations(stable[i], 2)]
+        plt.cla()
+        nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+        nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="g", width = 6 )
+        plt.pause(2)
+
+    for i in range(len(unstable)):
+        temp = [tuple(x) for x in itertools.combinations(unstable[i], 2)]
+        plt.cla()
+        nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+        nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="r", width = 6)
+        plt.pause(2)
--- user_defined_network.py
+++ user_defined_network.py
@@ -21,10 +21,12 @@ def checkStability(tris_list, G):
     unstableNum = len(unstable)
 
     balance_score = stableNum / (stableNum + unstableNum)
-    if balance_score >=  0.9:
-        print('The network is STABLE')
+    if balance_score == 1.0:
+        print('The network is STRUCTURALLY BALANCED') 
+    elif balance_score >=  0.9:
+        print('The network is APPROXIMATELY STRUCTURALLY BALANCED')
     else:
-        print('The network is UNSTABLE')
+        print('The network is Not STRUCTURALLY BALANCED')
 
     print("Stable triangles: ", stableNum)
     print("Unstable triangles: ", unstableNum)
@@ -32,55 +34,55 @@ def checkStability(tris_list, G):
     return stable, unstable
 
 if __name__ == "__main__":
-  G = nx.Graph()
+    G = nx.Graph()
+    
+    isPossible = False
+    while not isPossible:
+        n = int(input('Enter the number of nodes(>2): '))
+        isPossible = n > 2
+    
+    G.add_nodes_from([i for i in range(1, n+1)])
+    
+    mapping = {}
+    for i in range(1,n+1):
+        mapping[i] = "Node" + str(i)
+    G = nx.relabel_nodes(G, mapping)
 
-  n = int(input('Enter the number of nodes: '))
-  G.add_nodes_from([i for i in range(1, n+1)])
+    signs = ['+', '-']
+    for i in G.nodes():
+        for j in G.nodes():
+            if i != j:
+                G.add_edge(i, j, sign = random.choice(signs))
 
-  choice = 1
-  print("Enter the labels of the nodes one by one---->")
+    edges = G.edges()
+    nodes = G.nodes()
 
-  if(choice):
-    label_list = []
-    for i in range(1, n + 1):
-      label_list.append(input(f'Label for node {i}: '))
-    mapping = { i+1: label_list[i] for i in range(0, len(label_list)) }
-  G = nx.relabel_nodes(G, mapping)
-  signs = ['+', '-']
-  for i in G.nodes():
-      for j in G.nodes():
-          if i != j:
-              G.add_edge(i, j, sign = random.choice(signs))
+    tris_list = [list(x) for x in itertools.combinations(nodes, 3)]
+    stable, unstable = checkStability(tris_list, G)
 
-  edges = G.edges()
-  nodes = G.nodes()
+    edge_labels = nx.get_edge_attributes(G, 'sign')
+    pos = nx.circular_layout(G)
 
-  tris_list = [list(x) for x in itertools.combinations(nodes, 3)]
-  stable, unstable = checkStability(tris_list, G)
+    nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 15)
 
-  edge_labels = nx.get_edge_attributes(G, 'sign')
-  pos = nx.circular_layout(G)
-
-  nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-  nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 15)
-
-  Friends = [k for k, v in edge_labels.items() if v == '+']
-  Enemies = [k for k, v in edge_labels.items() if v == '-']
-  nx.draw_networkx_edges(G, pos, edgelist = Friends, edge_color="g")
-  nx.draw_networkx_edges(G, pos, edgelist = Enemies, edge_color="r")
+    Friends = [k for k, v in edge_labels.items() if v == '+']
+    Enemies = [k for k, v in edge_labels.items() if v == '-']
+    nx.draw_networkx_edges(G, pos, edgelist = Friends, edge_color="g")
+    nx.draw_networkx_edges(G, pos, edgelist = Enemies, edge_color="r")
   
-  plt.pause(10)
+    plt.pause(10)
 
-  for i in range(len(stable)):
-    temp = [tuple(x) for x in itertools.combinations(stable[i], 2)]
-    plt.cla()
-    nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-    nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="g", width = 6 )
-    plt.pause(2)
+    for i in range(len(stable)):
+        temp = [tuple(x) for x in itertools.combinations(stable[i], 2)]
+        plt.cla()
+        nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+        nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="g", width = 6 )
+        plt.pause(2)
 
-  for i in range(len(unstable)):
-    temp = [tuple(x) for x in itertools.combinations(unstable[i], 2)]
-    plt.cla()
-    nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-    nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="r", width = 6)
-    plt.pause(2)
+    for i in range(len(unstable)):
+        temp = [tuple(x) for x in itertools.combinations(unstable[i], 2)]
+        plt.cla()
+        nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+        nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="r", width = 6)
+        plt.pause(2)
