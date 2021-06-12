2102a376837c2d323ebdc82cf666070af3efd1a4
Hariprasath <hariprasathravi98@gmail.com>
2021-06-12 16:50:05 +0530

    Included a new case

--- manual_network.py
+++ manual_network.py
@@ -35,20 +35,47 @@ def checkStability(tris_list, G):
 
 if __name__ == "__main__":
     G = nx.Graph()
+    # Number of nodes
+    n = 6
+    
+    # G.add_nodes_from([i for i in range(1, n+1)])
 
-    n = 7
-    G.add_nodes_from([i for i in range(1, n+1)])
+    # Hard Coding positions
+    G.add_node(1,pos=(1,3))
+    G.add_node(2,pos=(1,2))
+    G.add_node(3,pos=(1,1))
+    G.add_node(4,pos=(4,3))
+    G.add_node(5,pos=(4,2))
+    G.add_node(6,pos=(4,1))
   
     mapping = {}
     for i in range(1,n+1):
         mapping[i] = "Node" + str(i)
     G = nx.relabel_nodes(G, mapping)
-
-    signs = ['+', '-']
-    for i in G.nodes():
-        for j in G.nodes():
-            if i != j:
-                G.add_edge(i, j, sign = random.choice(signs))
+    
+    # Hard coding the signs
+    G.add_edge( 'Node1', 'Node2', sign ='+')
+    G.add_edge( 'Node1', 'Node3', sign ='+')
+    G.add_edge( 'Node1', 'Node4', sign ='-')
+    G.add_edge( 'Node1', 'Node5', sign ='-')
+    G.add_edge( 'Node1', 'Node6', sign ='-')
+    G.add_edge( 'Node2', 'Node3', sign ='+')
+    G.add_edge( 'Node2', 'Node4', sign ='-')
+    G.add_edge( 'Node2', 'Node5', sign ='-')
+    G.add_edge( 'Node2', 'Node6', sign ='-')
+    G.add_edge( 'Node3', 'Node4', sign ='-')
+    G.add_edge( 'Node3', 'Node5', sign ='-')
+    G.add_edge( 'Node3', 'Node6', sign ='-')
+    G.add_edge( 'Node4', 'Node5', sign ='+')
+    G.add_edge( 'Node4', 'Node6', sign ='+')
+    G.add_edge( 'Node5', 'Node6', sign ='+')
+    
+    # # Random signs
+    # signs = ['+', '-']
+    # for i in G.nodes():
+    #     for j in G.nodes():
+    #         if i != j:
+    #             G.add_edge(i, j, sign = random.choice(signs))
 
     edges = G.edges()
     nodes = G.nodes()
@@ -57,7 +84,8 @@ if __name__ == "__main__":
     stable, unstable = checkStability(tris_list, G)
 
     edge_labels = nx.get_edge_attributes(G, 'sign')
-    pos = nx.circular_layout(G)
+    # pos = nx.circular_layout(G)
+    pos = nx.spring_layout(G)
 
     nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
     nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 15)
@@ -66,19 +94,22 @@ if __name__ == "__main__":
     Enemies = [k for k, v in edge_labels.items() if v == '-']
     nx.draw_networkx_edges(G, pos, edgelist = Friends, edge_color="g")
     nx.draw_networkx_edges(G, pos, edgelist = Enemies, edge_color="r")
-  
-    plt.pause(10)
+    
+    plt.show()
+    
+    # # Uncomment this to see individual triangle states
+    # plt.pause(10)
 
-    for i in range(len(stable)):
-        temp = [tuple(x) for x in itertools.combinations(stable[i], 2)]
-        plt.cla()
-        nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-        nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="g", width = 6 )
-        plt.pause(2)
+    # for i in range(len(stable)):
+    #     temp = [tuple(x) for x in itertools.combinations(stable[i], 2)]
+    #     plt.cla()
+    #     nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+    #     nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="g", width = 6 )
+    #     plt.pause(2)
 
-    for i in range(len(unstable)):
-        temp = [tuple(x) for x in itertools.combinations(unstable[i], 2)]
-        plt.cla()
-        nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
-        nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="r", width = 6)
-        plt.pause(2)
+    # for i in range(len(unstable)):
+    #     temp = [tuple(x) for x in itertools.combinations(unstable[i], 2)]
+    #     plt.cla()
+    #     nx.draw(G, pos, node_size = 2000, with_labels = True, font_size = 10)
+    #     nx.draw_networkx_edges(G, pos, edgelist = temp, edge_color="r", width = 6)
+    #     plt.pause(2)
